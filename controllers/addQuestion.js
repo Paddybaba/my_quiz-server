04382f5fc756e26c3b825f92920b2c5b56e0618a
unknown <paddybaba@gmail.com>
2021-11-28 11:39:03 +0530

    adding questions

--- controllers/addQuestion.js
+++ controllers/addQuestion.js
@@ -1,12 +1,24 @@
 const Question = require("../database/models/questionSchema");
+const Author = require("../database/models/authorSchema");
 
 async function addQuestion(req, res) {
   const question = req.body;
   console.log(question);
   try {
-    const writeresult = await Question.create({ question });
-    if (writeresult) res.status(200).json("Question added successfully");
-    else {
+    const writeresult = await Question.create(question);
+
+    if (writeresult) {
+      const authorExists = await Author.findOne({
+        authorname: question.author,
+      });
+      if (!authorExists) {
+        await Author.create({ authorname: question.author });
+      } else {
+        console.log("author is already present in database");
+      }
+
+      res.status(200).json("Question added successfully");
+    } else {
       console.log("Question not added");
     }
   } catch (err) {
