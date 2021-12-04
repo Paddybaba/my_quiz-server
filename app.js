b7268d7565984a09521df1624e9a2e5e11f58c6b
unknown <paddybaba@gmail.com>
2021-12-04 19:06:39 +0530

    CORS Present

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ dotenv.config({ path: "./config.env" });
 
 // require("./controllers/uploadFile");
 const app = express();
-
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 //////  MIDDLEWARES  ////////
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(require("./controllers/paths"));
