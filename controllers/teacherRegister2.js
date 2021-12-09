e56641ba4be48a98fa2ca842dea12fb5a764624a
unknown <paddybaba@gmail.com>
2021-12-09 20:58:00 +0530

    OAuth2

--- controllers/teacherRegister2.js
+++ controllers/teacherRegister2.js
@@ -2,12 +2,34 @@ const Teacher = require("../database/models/teacherSchema");
 const pendingTeacher = require("../database/models/pendingTeacher");
 const bcryt = require("bcryptjs");
 var nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  "42687148759-fsdq52ucbakq7da124q7kjcqnscp4at1.apps.googleusercontent.com",
+  "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",
+  "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({
+  refresh_token: process.env.MAIL_REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
 
 var transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
+    // user: "paddybaba@gmail.com",
+    // pass: process.env.GMAIL_PASS,
+    type: "OAuth2",
     user: "paddybaba@gmail.com",
-    pass: process.env.GMAIL_PASS,
+    clientId: process.env.MAIL_CLIENT_ID,
+    clientSecret: process.env.MAIL_SECRET_KEY,
+    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+    accessToken: accessToken,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
