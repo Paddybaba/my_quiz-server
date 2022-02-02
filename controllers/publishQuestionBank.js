4e7f25649016831b6b37fc5aee7c77eecb2eeb22
unknown <paddybaba@gmail.com>
2022-02-02 12:55:33 +0530

    getting Questiosn for teacher

--- controllers/publishQuestionBank.js
+++ controllers/publishQuestionBank.js
@@ -1,24 +1,22 @@
-const questionBank = require("../database/models/questionBank")
+const questionBank = require("../database/models/questionBank");
 
 async function publishQuestionBank(req, res) {
-    const qbProps = req.body
+  const qbProps = req.body;
 
-    try{
-        if(qbProps.teacher_id.length > 0){
-            const writeresult = await questionBank.create(qbProps);
-            console.log(writeresult)
-            res.status(200).json("Your question Bank published successfully")
-        }else{
-            res.status(400).json("Data insufficient")
-        }
-
-    }catch(err){
-        console.log(err.message)
-        res.err("Error publishing your question bank")
+  try {
+    if (qbProps.teacher_id.length > 0) {
+      const writeresult = await questionBank.create(qbProps);
+      console.log(writeresult);
+      res.status(200).json("Your question Bank published successfully");
+    } else {
+      res.status(400).json("Data insufficient");
     }
-
+  } catch (err) {
+    console.log(err.message);
+    res.err("Error publishing your question bank");
+  }
 }
 
 module.exports = {
-    publishQuestionBank : publishQuestionBank
-  }
+  publishQuestionBank: publishQuestionBank,
+};
