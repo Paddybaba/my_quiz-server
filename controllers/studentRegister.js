ace8088dad56cef4b382b65b33226ae9139e8f70
unknown <paddybaba@gmail.com>
2022-02-16 10:13:23 +0530

    adding token

--- controllers/studentRegister.js
+++ controllers/studentRegister.js
@@ -4,20 +4,40 @@ const bcryt = require("bcryptjs");
 
 async function registerStudent(req, res) {
   try {
+    // Get user input
     const { student_name, student_id, password } = req.body;
 
+    // Validate user input
+    if (!(student_name && student_id && password)) {
+      res.status(400).send("Data insufficient");
+    }
+
+    // Check user already registered
     const studentExists = await Student.findOne({ student_id: student_id });
     if (!studentExists) {
+      // Encrypt password
       const hash = await bcryt.hash(password, 10);
-      const writeresult = await Student.create({
+
+      // Create user in database
+      const student = await Student.create({
         student_id: student_id,
         student_name: student_name,
         password: hash,
       });
-      console.log("Student Added successfully :", writeresult._id.toString());
-      res.json("Registered Successfully");
+      console.log("Student Added successfully :", student._id.toString());
+
+      // Create token
+      const token = jwt.sign({ _id: student._id }, process.env.SECRET_KEY_JWT, {
+        expiresIn: "720h",
+      });
+
+      // save student token
+      student.token = token;
+
+      // return student to frontend
+      res.status(201).json(student);
     } else {
-      res.status(400).send("This id is already registered");
+      res.status(201).send("This id is already registered, proceed to login");
     }
   } catch (err) {
     console.log(err.message);
--- database/models/studentSchema.js
+++ database/models/studentSchema.js
@@ -5,6 +5,7 @@ const studentSchema = new mongoose.Schema({
   student_id: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
@@ -13,16 +14,19 @@ const studentSchema = new mongoose.Schema({
   student_name: {
     type: String,
   },
+  token: {
+    type: String,
+  },
 });
 
-studentSchema.methods.generateToken = async function () {
-  try {
-    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY_JWT);
-    return token;
-  } catch (err) {
-    console.log(err);
-  }
-};
+// studentSchema.methods.generateToken = async function () {
+//   try {
+//     let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY_JWT);
+//     return token;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 const Student = mongoose.model("Student", studentSchema);
 
 module.exports = Student;
